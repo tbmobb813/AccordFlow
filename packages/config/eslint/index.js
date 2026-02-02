df0ddc48c347cdae3efcb67677949fbd1d2c20b1
tbmobb813 <nixjason40@gmail.com>
2026-02-02 02:31:58 -0500

    feat: add shared ESLint and TypeScript configuration for the AccordFlow monorepo

---
+++ packages/config/eslint/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    // Shared rules can be customized here
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+  }
+};
